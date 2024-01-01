7c47e7f7296562f922b025276fc21dee8400fa82
Andrei Efremushkin <andrei.efr@gmail.com>
2024-01-02 01:45:54 +0200

    it'll run  over all files and folders in a given path and remove any unwanted suffixes from their names.

---
+++ myConstants.py
@@ -0,0 +1,12 @@
+WORDS_TO_REMOVE = ['1080p', '720p', 'WEB', '.web.', 'h264-', '[TGx]', 'AAC', 'BluRay', 'WEBRip',
+                   'DD5.1', 'DDP5.1', 'H.264', '-NTb', '2160p', 'x264', 'x265', 'ethel',
+                   'Galaxy', 'ETHEL', 'H264', '-SYNCOPY', 'GOSSIP'
+                                                          'AMZN', '.AMZN.', 'Rip.-TV', '-NHTFS', 'PCOK', '...',
+                   'HDTV', 'DVDRip', '[ettv]', '.AC3', 'HDRip',
+                   '-LOL', '-DL', '-FUM', '-KILLERS', '-MTB', 'XviD',
+                   '.800MB', '.900MB', '1600MB', 'Rip', '-RG', '.1400MB', '-EVO',
+                   '-AOC', '.3500MB', '-HUZZAH', '1400MB', '800MB', '-RARBG', '-SFM',
+                   '-TLA', '-RiVER', 'X264', '-BATV', '[eztv]', '-ORGANiC', '[rarbg]',
+                   '-SVA', '-MiNX', '-XXX', '( HEVC)']
+
+PATH_TO_PROCESS = 'Z:\\NEW VIDEO'
